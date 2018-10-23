a0c53afe51eaabdacba42be44394ae8055d9758c
Sasha Varlamov <sasha.varlamov@exlhub.com>
2018-10-23 12:15:19 -0700

    Fix userProfile query and lock down for instructors and/or self only

--- src/db-handlers/user/user-fetch.js
+++ src/db-handlers/user/user-fetch.js
@@ -136,6 +136,8 @@ export const fetchUserProfileById = async (obj_id, viewer, info) => {
         headline: 1,
         is_demo: 1,
         has_completed_first_tutorial: 1,
+        is_instructor: 1,
+        instructor_topics: 1,
         locales: 1,
         primary_locale: 1,
         avatar_url: 1,
